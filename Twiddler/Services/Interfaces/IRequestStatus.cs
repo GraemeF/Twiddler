bdcc62723d1a95eb86b2a65980be0fd05b8c04d4
Graeme Foster <graemef@gmail.com>
2010-05-26 22:22:51 +0100

    Added RequestMeter to StatusView.

--- Twiddler/Services/Interfaces/IRequestStatus.cs
+++ Twiddler/Services/Interfaces/IRequestStatus.cs
@@ -1,10 +1,13 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 
 namespace Twiddler.Services.Interfaces
 {
     public interface IRequestStatus : INotifyPropertyChanged
     {
-        int HourlyLimit { get; }
-        int RemainingHits { get; }
+        int HourlyLimit { get; set; }
+        int RemainingHits { get; set; }
+        DateTime PeriodEndTime { get; set; }
+        TimeSpan PeriodDuration { get; set; }
     }
 }
---
+++ Twiddler/Services/RequestStatus.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.ComponentModel;
+using Caliburn.Core.IoC;
+using Twiddler.Services.Interfaces;
+
+namespace Twiddler.Services
+{
+    [Singleton(typeof (IRequestStatus))]
+    public class RequestStatus : IRequestStatus
+    {
+        private int _hourlyLimit;
+        private TimeSpan _periodDuration;
+        private DateTime _periodEndTime;
+        private int _remainingHits;
+
+        #region IRequestStatus Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public int HourlyLimit
+        {
+            get { return _hourlyLimit; }
+            set
+            {
+                if (_hourlyLimit != value)
+                {
+                    _hourlyLimit = value;
+                    PropertyChanged.Raise(x => HourlyLimit);
+                }
+            }
+        }
+
+        public int RemainingHits
+        {
+            get { return _remainingHits; }
+            set
+            {
+                if (_remainingHits != value)
+                {
+                    _remainingHits = value;
+                    PropertyChanged.Raise(x => RemainingHits);
+                }
+            }
+        }
+
+        public DateTime PeriodEndTime
+        {
+            get { return _periodEndTime; }
+            set
+            {
+                if (_periodEndTime != value)
+                {
+                    _periodEndTime = value;
+                    PropertyChanged.Raise(x => PeriodEndTime);
+                }
+            }
+        }
+
+        public TimeSpan PeriodDuration
+        {
+            get { return _periodDuration; }
+            set
+            {
+                if (_periodDuration != value)
+                {
+                    _periodDuration = value;
+                    PropertyChanged.Raise(x => PeriodDuration);
+                }
+            }
+        }
+
+        #endregion
+    }
+}
