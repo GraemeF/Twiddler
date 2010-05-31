fcd72b082820ebd9b2f8381d8a4e0a13e81c40e0
Graeme Foster <graemef@gmail.com>
2010-05-31 17:28:46 +0100

    Added Gallio and build scripts to run code coverage.

--- Twiddler.Tests/Properties/AssemblyInfo.cs
+++ Twiddler.Tests/Properties/AssemblyInfo.cs
@@ -21,16 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("1e737868-60d9-46d9-a8ae-b5ad52efeaae")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
