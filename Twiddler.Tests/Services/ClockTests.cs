482f2bf7333de34bee64ceddf15d7b1c2b005166
Graeme Foster <graemef@gmail.com>
2010-05-31 18:04:30 +0100

    Added clock tests.

---
+++ Twiddler.Tests/Services/ClockTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Twiddler.Services;
+using Xunit;
+
+namespace Twiddler.Tests.Services
+{
+    public class ClockTests
+    {
+        [Fact]
+        public void GettingNow__ReturnsCurrentDateAndTimeInLocalTimeZone()
+        {
+            DateTime beforeNow = DateTime.Now;
+            DateTime clockNow = new Clock().Now;
+            DateTime afterNow = DateTime.Now;
+
+            Assert.InRange(clockNow, beforeNow, afterNow);
+        }
+    }
+}
