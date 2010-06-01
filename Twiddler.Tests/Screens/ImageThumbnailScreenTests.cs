33adc1de091900db61179f62fafc4d49f4d9edbc
Graeme Foster <graemef@gmail.com>
2010-06-01 13:15:46 +0100

    Added more tests.

--- Twiddler.Tests/Screens/ImageThumbnailScreenTests.cs
+++ Twiddler.Tests/Screens/ImageThumbnailScreenTests.cs
@@ -1,4 +1,5 @@
-﻿using Moq;
+﻿using System;
+using Moq;
 using Twiddler.Commands.Interfaces;
 using Twiddler.Models;
 using Twiddler.Screens;
@@ -8,19 +9,50 @@ namespace Twiddler.Tests.Screens
 {
     public class ImageThumbnailScreenTests
     {
+        private readonly ImageLocations _imageLocations = new ImageLocations
+                                                              {
+                                                                  FullSize = new Uri("http://full.size.image"),
+                                                                  Link = new Uri("http://link"),
+                                                                  Thumbnail = new Uri("http://thumbnail")
+                                                              };
+
         private readonly IOpenLinkCommand _openLinkCommand = new Mock<IOpenLinkCommand>().Object;
 
         [Fact]
         public void GettingOpenCommand__ReturnsCommand()
         {
-            var test = BuildDefaultTestSubject();
+            ImageThumbnailScreen test = BuildDefaultTestSubject();
 
             Assert.Same(_openLinkCommand, test.OpenLinkCommand);
         }
 
+        [Fact]
+        public void GettingLink__ReturnsLinkFromLocations()
+        {
+            ImageThumbnailScreen test = BuildDefaultTestSubject();
+
+            Assert.Equal(_imageLocations.Link, test.Link);
+        }
+
+        [Fact]
+        public void GettingThumbnail__ReturnsThumbnailFromLocations()
+        {
+            ImageThumbnailScreen test = BuildDefaultTestSubject();
+
+            Assert.Equal(_imageLocations.Thumbnail, test.Thumbnail);
+        }
+
+        [Fact]
+        public void GettingFullSize__ReturnsFullSizeFromLocations()
+        {
+            ImageThumbnailScreen test = BuildDefaultTestSubject();
+
+            Assert.Equal(_imageLocations.FullSize, test.FullSize);
+        }
+
         private ImageThumbnailScreen BuildDefaultTestSubject()
         {
-            return new ImageThumbnailScreen(new ImageLocations(), _openLinkCommand);
+            return new ImageThumbnailScreen(_imageLocations, _openLinkCommand);
         }
     }
 }
