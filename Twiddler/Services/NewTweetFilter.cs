551c4a33ef3bbb3425cbb3071a9cb3de5769c9c3
Graeme Foster <graemef@gmail.com>
2010-07-11 20:19:18 +0100

    Added NewTweetFilter.

---
+++ Twiddler/Services/NewTweetFilter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Caliburn.Core.IoC;
+using Twiddler.Core.Models;
+using Twiddler.Core.Services;
+
+namespace Twiddler.Services
+{
+    [PerRequest(typeof (INewTweetFilter))]
+    public class NewTweetFilter : INewTweetFilter
+    {
+        private readonly ITweetResolver _tweetResolver;
+
+        public NewTweetFilter(ITweetStore tweetResolver)
+        {
+            _tweetResolver = tweetResolver;
+        }
+
+        #region INewTweetFilter Members
+
+        public IEnumerable<Tweet> RemoveKnownTweets(IEnumerable<Tweet> tweets)
+        {
+            return tweets.Where(x => _tweetResolver.GetTweet(x.Id) == null);
+        }
+
+        #endregion
+    }
+}
