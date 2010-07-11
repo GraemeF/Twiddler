aaa1677b774e4f945551457a91e3d017c0cf6950
Graeme Foster <graemef@gmail.com>
2010-07-11 17:43:29 +0100

    Wait for stuff to happen before verification.

---
+++ Twiddler.TestData/Wait.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace Twiddler.TestData
+{
+    public class Wait
+    {
+        private static readonly TimeSpan DefaultDuration = TimeSpan.FromSeconds(5.0);
+        private static readonly TimeSpan DefaultPeriod = TimeSpan.FromSeconds(0.1);
+
+        public static void Until(Func<bool> condition)
+        {
+            DateTime endTime = DateTime.Now + DefaultDuration;
+            while (DateTime.Now < endTime)
+            {
+                Thread.Sleep(DefaultPeriod);
+                if (condition())
+                    return;
+            }
+
+            throw new ConditionNotMetException("The condition was not met within the allowed time.");
+        }
+    }
+}
