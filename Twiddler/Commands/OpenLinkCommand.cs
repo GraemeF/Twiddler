5f8dce7448c2e721b5e493da53635a240a98573a
Graeme Foster <graemef@gmail.com>
2010-09-03 14:38:26 +0100

    Fixed warnings.

--- Twiddler/Commands/OpenLinkCommand.cs
+++ Twiddler/Commands/OpenLinkCommand.cs
@@ -24,7 +24,9 @@ namespace Twiddler.Commands
             return true;
         }
 
+#pragma warning disable 0067
         public event EventHandler CanExecuteChanged;
+#pragma warning restore 0067
 
         #endregion
     }
